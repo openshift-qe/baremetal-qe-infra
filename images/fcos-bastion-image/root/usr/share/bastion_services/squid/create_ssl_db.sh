0c903e0e5628b17f4e758d4face746dab434f77f
Biagio Manzari <bmanzari@redhat.com>
2023-08-28 16:19:54 +0200

    apply review changes

--- images/fcos-bastion-image/root/usr/share/bastion_services/squid/create_ssl_db.sh
+++
@@ -1,4 +0,0 @@
-# SSL certificate generator for Squid fixes container runtime error
-# FATAL: The sslcrtd_program helpers are crashing too rapidly, need help!
-
-/usr/lib64/squid/security_file_certgen -c -s /var/lib/ssl_db -M 4
