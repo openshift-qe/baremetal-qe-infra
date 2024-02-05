88e193e596ac9915ac7d79154bda1b88340fe75f
Jad Haj Yahya <jhajyahy@redhat.com>
2024-02-05 15:46:10 +0200

    Code refactoring

--- images/fcos-bastion-image/root/usr/bin/clean-registries.sh
+++ images/fcos-bastion-image/root/usr/bin/clean-registries.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -x
 
-mapfile -t services < <(systemctl list-units --type service | grep -i "registry@" | awk '{print $1}' | awk -F'[@.]' '{print $2}')
-for port in "${services[@]}"; do
+mapfile -t ports < <(systemctl list-units 'registry@*' --no-pager --quiet | awk -F'[@.]' '{print $2}')
+for port in "${ports[@]}"; do
   disk_use=$(df /opt/registry-"${port}" --output='pcent' | grep -o '[0-9]*')
   if [ "$disk_use" -gt 85 ]; then
     rm -rf /opt/registry-"${port}"/data/docker/registry/v2/repositories/*
@@ -11,4 +11,3 @@ for port in "${services[@]}"; do
 done
 
 exit 0
-
